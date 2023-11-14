1ce547db56adfa046cacc17476c21516cde0fb2e
Andres Garza <hanato21@icloud.com>
2023-11-14 15:27:15 -0600

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 export default function App() {
   const [mode, setMode] = React.useState(true);
@@ -13,6 +14,7 @@ export default function App() {
     <div className="container">
       <Navbar darkMode={mode} toggleDarkMode={toggleDarkMode} />
       <Main darkMode={mode} />
+      <Footer />
     </div>
   );
 }
